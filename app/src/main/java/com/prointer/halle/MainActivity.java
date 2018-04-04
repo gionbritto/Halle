7a581614fa1a185e02577ccd08a9212875317d3a
diogo <diogobh93@gmail.com>
2018-04-03 22:26:14 -0300

    Modificações 22:26 03/04/2018

--- app/src/main/java/com/prointer/halle/MainActivity.java
+++ app/src/main/java/com/prointer/halle/MainActivity.java
@@ -1,13 +1,28 @@
 package com.prointer.halle;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    Button botaocadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaocadastro = (Button) findViewById(R.id.button_cadastrar);
+
+        botaocadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TelaCadastroActivity.class));
+            }
+        });
     }
 }
