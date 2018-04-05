44b49e50b6fa51166c88654b63542bd2ad627ad3
diogo <diogobh93@gmail.com>
2018-04-04 23:14:02 -0300

    Modificações 23:13 04/04/2018

--- app/src/main/java/com/prointer/halle/TelaCadastroActivity.java
+++ app/src/main/java/com/prointer/halle/TelaCadastroActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +19,7 @@ public class TelaCadastroActivity extends Activity {
     EditText camposenha;
     Button botaosalvarcadastro;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,10 @@ public class TelaCadastroActivity extends Activity {
         campoemail = (EditText) findViewById(R.id.campo_cadastro_email);
         camposenha = (EditText) findViewById(R.id.campo_cadastro_senha);
         botaosalvarcadastro = (Button) findViewById(R.id.button_salvar);
+
+        //Criando o banco de dados
+
+
 
 
         botaosalvarcadastro.setOnClickListener(new View.OnClickListener() {
@@ -41,7 +47,6 @@ public class TelaCadastroActivity extends Activity {
 
                 try {
 
-                    //Criando o banco de dados
                     SQLiteDatabase banco = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
                     //Criando as tabelas
@@ -54,13 +59,41 @@ public class TelaCadastroActivity extends Activity {
                     banco.execSQL("INSERT INTO pessoas (nome, email, senha) VALUES ('camponome','campoemail','camposenha')");
 
 
+                    //Criando um cursos para pesquisa nas colunas, ele para no última linha da coluna, é necessário retornar o cursos.
+                    Cursor cursor = banco.rawQuery("SELECT * FROM usuario ", null);
+
+                    //Recuperar o índice da coluna
+                    int indicecolunanome = cursor.getColumnIndex("nome");
+                    int indicecolunaemail = cursor.getColumnIndex("email");
+                    int indicecolunasenha = cursor.getColumnIndex("senha");
+                    int indicecolunaid = cursor.getColumnIndex("id");
+
+
+                    //Voltando o cursor para a primeira linha da coluna
+                    cursor.moveToFirst();
+
+
+                    while (cursor != null) {
+
+                /*Esses códigos abaixo serve para recuperar as informações da coluna*/
+
+                        cursor.getString(indicecolunanome);
+
+                        Toast.makeText(TelaCadastroActivity.this, cursor.getString(indicecolunanome), Toast.LENGTH_SHORT).show();
+
+                        //Movendo o cursos para a próxima linha
+                        cursor.moveToNext();
+
+                    }
+
+
+
+
                 } catch (Exception e) {
 
                     e.printStackTrace();
 
                 }
-
-                Toast.makeText(TelaCadastroActivity.this, "GRAVOU, TESTAR INFO NO BANCO", Toast.LENGTH_SHORT).show();
 
 
             }
